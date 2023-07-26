9865e0383c9d17a41fbf54ca5fc83a8bbbfbe7e4
Sey My <Seymy@camma.com>
2023-07-26 08:12:15 +0700

    update seederPosistion

--- database/migrations/2023_01_17_093320_create_positions_table.php
+++ database/migrations/2023_01_17_093320_create_positions_table.php
@@ -17,6 +17,7 @@ class CreatePositionsTable extends Migration
             $table->id();
             $table->string('name_khmer');
             $table->string('name_english');
+            $table->integer('parent_id')->nullable();
             $table->bigInteger('created_by')->unsigned()->nullable();
             $table->bigInteger('updated_by')->unsigned()->nullable();
             $table->dateTime('deleted_at')->nullable();
