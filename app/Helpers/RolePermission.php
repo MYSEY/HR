191e93d32d8044c30fd831f4f272cdea0d7ac532
HovHong <52909532+HovHong@users.noreply.github.com>
2023-11-14 14:39:34 +0700

    set access permission in dashboard

--- app/Helpers/RolePermission.php
+++ app/Helpers/RolePermission.php
@@ -16,7 +16,7 @@ function RolePermission()
 {
     $id=Auth::user()->role_id;
     $role_permission = permissions::where('role_id',$id)->get()->toArray();
-    if(count($role_permission) == 0){
+    if(count($role_permission) < 0){
         return false;
     }
     $datas = new dataMenu();
@@ -45,6 +45,40 @@ class dataRolePermission{
     public $checkbox;
     public $is_dashboard;
 }
+function permissionAccess($menu_id,$name_button){
+    $id=Auth::user()->role_id;
+    $permission = permissions::where('role_id',$id)->get()->toArray();
+    $arrayPermissions = [];
+    foreach ($permission as $row) {
+        $arrayPermissions[$row["menu_id"]] = $row;
+    }
+    $dataObject = new dataRolePermission();
+    if (array_key_exists($menu_id,$arrayPermissions) && $arrayPermissions[$menu_id][$name_button]){
+        if ($menu_id == "2") {
+            $dataObject->is_dashboard = json_decode($arrayPermissions[$menu_id][$name_button], true);
+        }else{
+            $dataObject->value = $arrayPermissions[$menu_id][$name_button];
+        }
+    } else{
+        $dataJson = [
+            "is_employee"=>"0",
+            "is_age_of_employee"=>"0",
+            "is_birthday_reminder"=>"0",
+            "is_total_number_of_staff"=>"0",
+            "is_total_inactive_staff"=>"0",
+            "is_resigned_staff"=>"0",
+            "is_reasons_of_staff"=>"0",
+            "is_staff_ratio"=>"0",
+            "is_staff_taking_leave"=>"0",
+            "is_staff_training_internal"=>"0",
+            "is_staff_training_external"=>"0"
+        ];
+        $dataObject->value = 0;
+        $dataObject->is_dashboard = $dataJson;
+    }
+    return $dataObject;
+}
+
 function SetCheckbox($datas,$permission_name,$permission_checkbox){
     $dataObject = new dataRolePermission();
     if (array_key_exists($permission_name,$datas) && $datas[$permission_name][$permission_checkbox]){
