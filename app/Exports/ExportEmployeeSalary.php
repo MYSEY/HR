c9898e7afe88f49c95c9a22120cb76c2f3d6066f
MYSEY <52909452+MYSEY@users.noreply.github.com>
2024-03-15 16:31:22 +0700

    update

--- app/Exports/ExportEmployeeSalary.php
+++ app/Exports/ExportEmployeeSalary.php
@@ -87,7 +87,7 @@ class ExportEmployeeSalary implements FromCollection, WithColumnWidths, WithHead
         })
         ->when($yearLy, function ($query, $yearLy) {
             $query->whereYear('payment_date', $yearLy);
-        })->orderBy('employee_id')->get();
+        })->orderBy('id','DESC')->get();
         $dataExport = [];
         $i = 0;
         foreach ($payroll as $value) {
