6e151633e6733fcb68b636c4988d12b63f07b684
MYSEY <52909452+MYSEY@users.noreply.github.com>
2024-07-10 11:28:14 +0700

    update dashboad

--- app/Http/Controllers/Admins/DashboadController.php
+++ app/Http/Controllers/Admins/DashboadController.php
@@ -29,6 +29,10 @@ class DashboadController extends Controller
         return view('dashboads.employee',compact('data','holiday','LeaveRequest'));
     }
     public function dashboadAdmin(){
+        $dataContract = '';
+        $dataUpComming = '';
+        $dataProbation = '';
+        $dataShortList = '';
         if (Auth::user()->RolePermission == 'admin' || Auth::user()->RolePermission == 'HR' || Auth::user()->RolePermission == 'developer' || Auth::user()->RolePermission == 'BOD' || Auth::user()->RolePermission == 'CEO') {
             $dataShortList = DB::table('candidate_resumes')->select('candidate_resumes.*')
             ->where(DB::raw("(DATE_FORMAT(candidate_resumes.interviewed_date,'%Y-%m-%d'))"), Carbon::now()->format('Y-m-d'))
