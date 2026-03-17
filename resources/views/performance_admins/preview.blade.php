6bee21e61ef73517b380a35b545e9eccb33f5676
Hong Hov <Honghov@camma.com>
2026-03-17 08:27:51 +0700

    update kpi on bbutton submit

--- resources/views/performance_admins/preview.blade.php
+++ resources/views/performance_admins/preview.blade.php
@@ -151,20 +151,20 @@
                                     @endphp
                                     @foreach ($data->titles as $item)
                                         <tr>
-                                            <td colspan="2" class="text-center">
-                                                <input type="text" class="form-control" value="{{ $item->title ?? '' }}" required>
+                                            <td colspan="4" class="text-center">
+                                                <input type="text" style="background: #efa781" class="form-control" value="{{ $item->title ?? '' }}" required>
                                             </td>
-                                            <td colspan="1" class="text-center"></td>
-                                            <td colspan="1" class="text-center"></td>
+                                            {{-- <td colspan="1" class="text-center"></td> --}}
+                                            {{-- <td colspan="1" class="text-center"></td> --}}
                                         </tr>
                                         
                                         @foreach ($item->purposes as $purposeItem)
                                             <tr>
-                                                <td colspan="2" class="text-center">
-                                                    <input type="text" class="form-control" value="{{ $purposeItem->name ?? '' }}" required>
+                                                <td colspan="4" class="text-center">
+                                                    <input type="text" style="background: #f0cc9b" class="form-control" value="{{ $purposeItem->name ?? '' }}" required>
                                                 </td>
-                                                <td colspan="1" class="text-center"></td>
-                                                <td colspan="1" class="text-center"></td>
+                                                {{-- <td colspan="1" class="text-center"></td> --}}
+                                                {{-- <td colspan="1" class="text-center"></td> --}}
                                             </tr>
                                             
                                             @foreach ($purposeItem->performanceDetail as $Detailitem)
